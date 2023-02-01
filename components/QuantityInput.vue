f07681e42fa1d9cd872d73a938dcfa150be59da4
Trevor Thiessen <treviguy57@gmail.com>
2023-02-01 10:56:42 -0500

    Quantity input component (#5)

--- components/QuantityInput.vue
+++ components/QuantityInput.vue
@@ -1,16 +1,31 @@
 <template>
-  <div class="mb-2 flex">
-    <div class="rounded-l bg-slate-700 px-2 py-1 text-slate-50">Quantity</div>
-    <input
-      v-model="quantity"
-      type="number"
-      class="w-full rounded-r border px-2 py-1 shadow-inner"
-    />
+  <div>
+    <div class="flex">
+      <div class="rounded-l bg-slate-700 px-2 py-1 text-slate-50">Quantity</div>
+      <input
+        v-model="quantity"
+        type="number"
+        class="w-full rounded-r border px-2 py-1 shadow-inner focus:outline-none"
+        :class="quantityState ? 'border-red-500' : ''"
+      />
+    </div>
+
+    <p class="ml-1 text-sm text-red-500">
+      {{ quantityState }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import type { Ref } from 'vue'
 
-const quantity = ref(1)
+const quantity: Ref<number | string> = ref(1)
+const quantityState = computed(() => {
+  if (quantity.value === '') return 'Quantity is required.'
+  if (quantity.value <= 0) return 'Quantity must be more than 0.'
+  if (parseInt(`${quantity.value}`) !== quantity.value)
+    return 'Quantity must be a whole number.'
+  return ''
+})
 </script>
--- components/quantity-input.spec.ts
+++ components/quantity-input.spec.ts
@@ -7,4 +7,25 @@ describe('Quantity Input', () => {
     const wrapper = mount(QuantityInput)
     expect(wrapper.text()).toContain('Quantity')
   })
+
+  it('should say quantity is required if input is blank', async () => {
+    const wrapper = mount(QuantityInput)
+    const input = wrapper.find('input')
+    await input.setValue('')
+    expect(wrapper.text()).toContain('is required.')
+  })
+
+  it('should say quantity must be more than 0 if input is 0', async () => {
+    const wrapper = mount(QuantityInput)
+    const input = wrapper.find('input')
+    await input.setValue(0)
+    expect(wrapper.text()).toContain('must be more than 0.')
+  })
+
+  it('should say quantity must be a whole number if input is a float', async () => {
+    const wrapper = mount(QuantityInput)
+    const input = wrapper.find('input')
+    await input.setValue(1.5)
+    expect(wrapper.text()).toContain('must be a whole number.')
+  })
 })
