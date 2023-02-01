f3971b9795630864fef8362bb70ae431e4decaf6
Trevor <treviguy57@gmail.com>
2023-02-01 14:23:25 -0500

    change names

--- components/QuantityInput.vue
+++ components/QuantityInput.vue
@@ -6,13 +6,13 @@
         :value="value"
         type="number"
         class="w-full rounded-r border px-2 py-1 shadow-inner focus:outline-none"
-        :class="state ? 'border-red-500' : ''"
+        :class="!isValid ? 'border-red-500' : ''"
         @input="emit('update:value', ($event.target as HTMLInputElement).value)"
       />
     </div>
 
-    <p v-if="!state" class="ml-1 text-sm text-red-500">
-      {{ stateMessage }}
+    <p v-if="!isValid" class="ml-1 text-sm text-red-500">
+      {{ message }}
     </p>
   </div>
 </template>
@@ -22,15 +22,15 @@ import { computed, watch } from 'vue'
 
 const props = defineProps<{
   value: number | string
-  state: boolean
+  isValid: boolean
 }>()
 
 const emit = defineEmits<{
   (e: 'update:value', value: number | string): void
-  (e: 'update:state', value: boolean): void
+  (e: 'update:isValid', value: boolean): void
 }>()
 
-const stateMessage = computed(() => {
+const message = computed(() => {
   if (props.value === '') return 'Quantity is required.'
   if (props.value <= 0) return 'Quantity must be more than 0.'
   if (parseInt(`${props.value}`) !== props.value)
@@ -38,5 +38,5 @@ const stateMessage = computed(() => {
   return ''
 })
 
-watch(stateMessage, () => emit('update:state', !!stateMessage.value))
+watch(message, () => emit('update:isValid', !message.value))
 </script>
--- components/quantity-input.spec.ts
+++ components/quantity-input.spec.ts
@@ -4,23 +4,27 @@ import QuantityInput from './QuantityInput.vue'
 
 describe('Quantity Input', () => {
   it('should include the word Quantity', () => {
-    const wrapper = mount(QuantityInput, { props: { value: 1, state: true } })
+    const wrapper = mount(QuantityInput, { props: { value: 1, isValid: true } })
     expect(wrapper.text()).toContain('Quantity')
   })
 
   it('should say quantity is required if input is blank', async () => {
-    const wrapper = mount(QuantityInput, { props: { value: '', state: false } })
+    const wrapper = mount(QuantityInput, {
+      props: { value: '', isValid: false },
+    })
     expect(wrapper.text()).toContain('is required.')
   })
 
   it('should say quantity must be more than 0 if input is 0', async () => {
-    const wrapper = mount(QuantityInput, { props: { value: 0, state: false } })
+    const wrapper = mount(QuantityInput, {
+      props: { value: 0, isValid: false },
+    })
     expect(wrapper.text()).toContain('must be more than 0.')
   })
 
   it('should say quantity must be a whole number if input is a float', async () => {
     const wrapper = mount(QuantityInput, {
-      props: { value: 1.5, state: false },
+      props: { value: 1.5, isValid: false },
     })
     expect(wrapper.text()).toContain('must be a whole number.')
   })
