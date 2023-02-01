e683c7a2a6de267a4c959ecad179ba24cd164f2f
Trevor <treviguy57@gmail.com>
2023-02-01 14:19:44 -0500

    working with two v-models

--- components/LengthInput.vue
+++ components/LengthInput.vue
@@ -3,25 +3,36 @@
     <div class="flex">
       <div class="rounded-l bg-slate-700 px-2 py-1 text-slate-50">Length</div>
       <input
-        v-model="length"
+        :value="value"
         type="number"
         class="w-full rounded-r border px-2 py-1 shadow-inner focus:outline-none"
-        :class="lengthState ? 'border-red-500' : ''"
+        :class="state ? 'border-red-500' : ''"
+        @input="emit('update:value', ($event.target as HTMLInputElement).value)"
       />
     </div>
 
-    <p class="ml-1 text-sm text-red-500">{{ lengthState }}</p>
+    <p v-if="!state" class="ml-1 text-sm text-red-500">{{ stateMessage }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import type { Ref } from 'vue'
+import { computed, watch } from 'vue'
 
-const length: Ref<number | string> = ref(1)
-const lengthState = computed(() => {
-  if (length.value === '') return 'Length is required.'
-  if (length.value <= 0) return 'Length must be more than 0.'
+const props = defineProps<{
+  value: number | string
+  state: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:value', value: number | string): void
+  (e: 'update:state', value: boolean): void
+}>()
+
+const stateMessage = computed(() => {
+  if (props.value === '') return 'Length is required.'
+  if (props.value <= 0) return 'Length must be more than 0.'
   return ''
 })
+
+watch(stateMessage, () => emit('update:state', !!stateMessage.value))
 </script>
