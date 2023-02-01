d28e168d6544813d0a2f464d79107bedc927acbe
Trevor <treviguy57@gmail.com>
2023-02-01 14:24:55 -0500

    change to isValid

--- components/length-input.spec.ts
+++ components/length-input.spec.ts
@@ -4,17 +4,17 @@ import LengthInput from './LengthInput.vue'
 
 describe('Length Input', () => {
   it('should include the word Length', () => {
-    const wrapper = mount(LengthInput, { props: { value: 1, state: true } })
+    const wrapper = mount(LengthInput, { props: { value: 1, isValid: true } })
     expect(wrapper.text()).toContain('Length')
   })
 
   it('should say length is required if input is blank', async () => {
-    const wrapper = mount(LengthInput, { props: { value: '', state: false } })
+    const wrapper = mount(LengthInput, { props: { value: '', isValid: false } })
     expect(wrapper.text()).toContain('is required.')
   })
 
   it('should say length must be more than 0 if input is 0', async () => {
-    const wrapper = mount(LengthInput, { props: { value: 0, state: false } })
+    const wrapper = mount(LengthInput, { props: { value: 0, isValid: false } })
     expect(wrapper.text()).toContain('must be more than 0.')
   })
 })
