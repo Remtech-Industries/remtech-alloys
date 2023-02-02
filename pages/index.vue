7bf43d6fec0d02cb38ae4983a29bdd7806f863b5
John Neusteter <jneusteter@remtechindustries.com>
2023-02-02 13:42:50 -0500

    get collections

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,27 @@
   <div class="flex">
     <CollectionSidebar />
 
-    <table class="text-slate-700">
-      <thead>
-        <tr class="border-b">
-          <th class="border-r py-1">Name</th>
-          <th class="py-1"># of Products</th>
-        </tr>
-      </thead>
+    <div class="p-3">
+      <table class="text-slate-700">
+        <thead>
+          <tr class="border-b">
+            <th class="border-r py-1">Name</th>
+            <th class="py-1"># of Products</th>
+          </tr>
+        </thead>
 
-      <tbody>
-        <tr class="border-b" v-for="{ node } in collections">
-          <td class="border-r pr-6">
-            <NuxtLink :to="`/collections/${node.handle}`">
-              {{ node.title }}
-            </NuxtLink>
-          </td>
-          <td>{{ node.products.edges.length }}</td>
-        </tr>
-      </tbody>
-    </table>
+        <tbody>
+          <tr class="border-b" v-for="{ node } in collections">
+            <td class="border-r pr-6">
+              <NuxtLink :to="`/collections/${node.handle}`">
+                {{ node.title }}
+              </NuxtLink>
+            </td>
+            <td>{{ node.products.edges.length }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
