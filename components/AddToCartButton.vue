1ee9a14f39de6f8b3e15a0cd9e6e2aecdbdc61d3
John Neusteter <jneusteter@remtechindustries.com>
2023-02-03 10:00:55 -0500

    get cart

--- components/AddToCartButton.vue
+++ components/AddToCartButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    @click="addToCart()"
     class="rounded bg-slate-400 px-2 py-1 text-slate-700 hover:bg-slate-300"
   >
     Add To Cart
@@ -7,5 +8,41 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ form: object }>()
+import { usePostToShopify } from '~~/utils/post-to-shopify'
+import { useGetCart } from '~~/utils/get-cart'
+interface Props {
+  form: object
+  selectedVariant: object
+}
+const props = defineProps<Props>()
+
+const query = `
+mutation createCart($cartInput: CartInput) {
+  cartCreate(input: $cartInput) {
+    cart {
+      id
+    }
+  }
+}
+`
+// {"data":{"cartCreate":{"cart":{"id":"gid://shopify/Cart/8c1440386e1e325a270b62e92a00e0ee"}}}}
+
+const cartId = 'gid://shopify/Cart/8c1440386e1e325a270b62e92a00e0ee'
+
+const cart = await useGetCart(cartId)
+
+function addToCart() {
+  const response = usePostToShopify(query, {
+    cartInput: {
+      lines: [
+        {
+          quantity: props.form.quantity,
+          merchandiseId: props.selectedVariant.id,
+        },
+      ],
+    },
+  })
+
+  console.log(response)
+}
 </script>
