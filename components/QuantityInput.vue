b3472dadadfc9c9b93ca91d764af7e29ab327aa2
John Neusteter <jneusteter@gmail.com>
2023-02-06 14:12:02 -0500

    quantity input tests (#16)
    Co-authored-by: John Neusteter <jneusteter@remtechindustries.com>

--- components/QuantityInput.vue
+++ components/QuantityInput.vue
@@ -3,37 +3,43 @@
     <div class="rounded-l bg-slate-700 px-2 py-1 text-slate-50">Quantity</div>
 
     <input
-      :value="value"
       type="number"
       class="w-full rounded-r border px-2 py-1 shadow-inner focus:outline-none"
-      :class="!isValid ? 'border-red-500' : ''"
-      @input="emit('update:value', ($event.target as HTMLInputElement).value)"
+      :class="message ? 'border-red-500' : ''"
+      @input="onInput($event)"
     />
   </div>
 
-  <p v-if="!isValid" class="ml-1 text-sm text-red-500">{{ message }}</p>
+  <p v-if="message" class="ml-1 text-sm text-red-500">{{ message }}</p>
 </template>
 
 <script setup lang="ts">
-import { computed, watch } from 'vue'
+import { ref } from 'vue'
 
-const props = defineProps<{
-  value: number | string
-  isValid: boolean
-}>()
+const message = ref('')
 
 const emit = defineEmits<{
-  (e: 'update:value', value: number | string): void
+  (e: 'update:quantity', value: number): void
   (e: 'update:isValid', value: boolean): void
 }>()
 
-const message = computed(() => {
-  if (props.value === '') return 'Quantity is required.'
-  if (props.value <= 0) return 'Quantity must be more than 0.'
-  if (parseInt(`${props.value}`) !== props.value)
-    return 'Quantity must be a whole number.'
-  return ''
-})
+function onInput(event: Event) {
+  message.value = ''
+  const quantity = (event.target as HTMLInputElement).value
 
-watch(message, () => emit('update:isValid', !message.value))
+  if (quantity === '') {
+    message.value = 'Quantity is required.'
+    emit('update:isValid', false)
+    return
+  }
+
+  if (+quantity <= 0) {
+    message.value = 'Quantity must be greater than 0.'
+    emit('update:isValid', false)
+    return
+  }
+
+  emit('update:quantity', +quantity)
+  emit('update:isValid', true)
+}
 </script>
--- components/quantity-input.spec.ts
+++ components/quantity-input.spec.ts
@@ -4,28 +4,71 @@ import QuantityInput from './QuantityInput.vue'
 
 describe('Quantity Input', () => {
   it('should include the word Quantity', () => {
-    const wrapper = mount(QuantityInput, { props: { value: 1, isValid: true } })
+    const wrapper = mount(QuantityInput)
     expect(wrapper.text()).toContain('Quantity')
   })
 
+  it('should display no errors and emit value when valid', async () => {
+    const wrapper = mount(QuantityInput)
+    const $input = wrapper.get('input')
+    $input.element.value = '1'
+    await $input.trigger('input')
+
+    expect(wrapper.text()).not.toContain('is required.')
+    expect($input.classes()).not.toContain('border-red-500')
+    expect(wrapper.emitted('update:quantity')).toEqual([[1]])
+    expect(wrapper.emitted('update:isValid')).toEqual([[true]])
+  })
+
   it('should say quantity is required if input is blank', async () => {
-    const wrapper = mount(QuantityInput, {
-      props: { value: '', isValid: false },
-    })
+    const wrapper = mount(QuantityInput)
+    const $input = wrapper.get('input')
+    $input.element.value = ''
+    await $input.trigger('input')
+
     expect(wrapper.text()).toContain('is required.')
+    expect($input.classes()).toContain('border-red-500')
+    expect(wrapper.emitted('update:quantity')).toBeUndefined()
+    expect(wrapper.emitted('update:isValid')).toEqual([[false]])
   })
 
-  it('should say quantity must be more than 0 if input is 0', async () => {
-    const wrapper = mount(QuantityInput, {
-      props: { value: 0, isValid: false },
-    })
-    expect(wrapper.text()).toContain('must be more than 0.')
+  it('should say quantity is required if input is 0', async () => {
+    const wrapper = mount(QuantityInput)
+    const $input = wrapper.get('input')
+    $input.element.value = '0'
+    await $input.trigger('input')
+
+    expect(wrapper.text()).toContain('greater than 0.')
+    expect($input.classes()).toContain('border-red-500')
+    expect(wrapper.emitted('update:quantity')).toBeUndefined()
+    expect(wrapper.emitted('update:isValid')).toEqual([[false]])
   })
 
-  it('should say quantity must be a whole number if input is a float', async () => {
-    const wrapper = mount(QuantityInput, {
-      props: { value: 1.5, isValid: false },
-    })
-    expect(wrapper.text()).toContain('must be a whole number.')
+  it('should say quantity is required to be more than 0 if user enters negative number', async () => {
+    const wrapper = mount(QuantityInput)
+    const $input = wrapper.get('input')
+    $input.element.value = '-1'
+    await $input.trigger('input')
+
+    expect(wrapper.text()).toContain('greater than 0.')
+    expect($input.classes()).toContain('border-red-500')
+    expect(wrapper.emitted('update:quantity')).toBeUndefined()
+    expect(wrapper.emitted('update:isValid')).toEqual([[false]])
+  })
+
+  it('should reset the error when user corrects the input', async () => {
+    const wrapper = mount(QuantityInput)
+    const $input = wrapper.get('input')
+    $input.element.value = '0'
+    await $input.trigger('input')
+
+    expect(wrapper.text()).toContain('greater than 0.')
+    expect($input.classes()).toContain('border-red-500')
+
+    $input.element.value = '1'
+    await $input.trigger('input')
+
+    expect(wrapper.text()).not.toContain('greater than 0.')
+    expect($input.classes()).not.toContain('border-red-500')
   })
 })
