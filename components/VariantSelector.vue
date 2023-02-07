78c326e5fb2771592b87ee342693e252375f42b5
John Neusteter <jneusteter@gmail.com>
2023-02-07 07:53:46 -0500

    extract variant selector (#18)
    Co-authored-by: John Neusteter <jneusteter@remtechindustries.com>

---
+++ components/VariantSelector.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="rounded border border-slate-300 p-2">
+    <div>{{ useDisplayHeatNumber(variant.title) }}</div>
+
+    <div class="text-xs font-thin">
+      {{ variant.quantityAvailable }}
+      {{ variant.priceV2.amount }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDisplayHeatNumber } from '~~/utils/display-heat-number'
+import type { Variant } from '~~/utils/types'
+
+interface Props {
+  variant: Variant
+}
+
+const props = defineProps<Props>()
+</script>
