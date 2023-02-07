102724fa35ba7a9764dbc9a3d06f3f08c0a83bbb
Trevor <treviguy57@gmail.com>
2023-02-07 09:35:15 -0500

    update component

--- components/LengthInput.vue
+++ components/LengthInput.vue
@@ -3,35 +3,43 @@
     <div class="rounded-l bg-slate-700 px-2 py-1 text-slate-50">Length</div>
 
     <input
-      :value="value"
       type="number"
       class="w-full rounded-r border px-2 py-1 shadow-inner focus:outline-none"
-      :class="!isValid ? 'border-red-500' : ''"
-      @input="emit('update:value', ($event.target as HTMLInputElement).value)"
+      :class="message ? 'border-red-500' : ''"
+      @input="onInput($event)"
     />
   </div>
 
-  <p v-if="!isValid" class="ml-1 text-sm text-red-500">{{ message }}</p>
+  <p v-if="message" class="ml-1 text-sm text-red-500">{{ message }}</p>
 </template>
 
 <script setup lang="ts">
-import { computed, watch } from 'vue'
+import { ref } from 'vue'
 
-const props = defineProps<{
-  value: number | string
-  isValid: boolean
-}>()
+const message = ref('')
 
 const emit = defineEmits<{
-  (e: 'update:value', value: number | string): void
+  (e: 'update:length', value: number): void
   (e: 'update:isValid', value: boolean): void
 }>()
 
-const message = computed(() => {
-  if (props.value === '') return 'Length is required.'
-  if (props.value <= 0) return 'Length must be more than 0.'
-  return ''
-})
+function onInput(event: Event) {
+  message.value = ''
+  const length = (event.target as HTMLInputElement).value
 
-watch(message, () => emit('update:isValid', !message.value))
+  if (length === '') {
+    message.value = 'Length is required.'
+    emit('update:isValid', false)
+    return
+  }
+
+  if (+length <= 0) {
+    message.value = 'Length must be greater than 0.'
+    emit('update:isValid', false)
+    return
+  }
+
+  emit('update:length', +length)
+  emit('update:isValid', true)
+}
 </script>
