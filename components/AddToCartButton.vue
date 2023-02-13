c56f1107b9a797bfdc3c209155db53ed973b4730
John Neusteter <jneusteter@gmail.com>
2023-02-13 15:45:03 -0500

    add disabled attribute (#24)
    Co-authored-by: John Neusteter <jneusteter@remtechindustries.com>

--- components/AddToCartButton.vue
+++ components/AddToCartButton.vue
@@ -1,7 +1,11 @@
 <template>
   <button
+    :disabled="disabled"
     @click="addToCart(items)"
     class="rounded bg-slate-400 px-2 py-1 text-slate-700 hover:bg-slate-300"
+    :class="
+      disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer opacity-100'
+    "
   >
     Add To Cart
   </button>
@@ -10,7 +14,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useCartStore } from '~~/stores/cart'
-import { Form, Variant } from '~~/utils/types'
+import type { Form, Variant } from '~~/utils/types'
 
 const { addToCart } = useCartStore()
 
@@ -19,6 +23,10 @@ interface Props {
   selectedVariant: Variant
 }
 const props = defineProps<Props>()
+
+const disabled = computed(() => {
+  return !props.form.quantity || !props.selectedVariant || !props.form.length
+})
 
 const items = computed(() => {
   return [
