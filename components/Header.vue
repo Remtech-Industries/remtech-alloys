fe824e7d2ec8b8fcb354a0bb61720babb587a231
Trevor <treviguy57@gmail.com>
2023-02-15 14:18:27 -0500

    show item count

--- components/Header.vue
+++ components/Header.vue
@@ -4,15 +4,18 @@
       <img src="/logo.png" width="200px" />
     </NuxtLink>
 
-    <NuxtLink to="/cart" class="font-semibold text-slate-700">
-      Cart
-      <span class="rounded bg-slate-600 px-1 text-slate-100">a</span>
+    <NuxtLink to="/cart" class="flex gap-1 font-semibold text-slate-700">
+      <p>Cart</p>
+
+      <p class="rounded bg-slate-600 px-1 text-slate-100">
+        {{ itemCount }}
+      </p>
     </NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
-// import { useCartStore } from '@/stores/cart'
-// import { storeToRefs } from 'pinia'
-// const { itemCount } = storeToRefs(useCartStore())
+import { storeToRefs } from 'pinia'
+import { useCartStore } from '@/stores/cart'
+const { itemCount } = storeToRefs(useCartStore())
 </script>
