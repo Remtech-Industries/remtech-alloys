d6fe3a7db3917e2961b8124448b6efe2a4dcebe2
Trevor Thiessen <treviguy57@gmail.com>
2023-04-26 15:41:35 -0400

    show message when item is added (#34)
    * show message when item is added
    
    * spell word
    
    ---------
    
    Co-authored-by: John Neusteter <jneusteter@gmail.com>

--- components/Header.vue
+++ components/Header.vue
@@ -5,6 +5,10 @@
     </NuxtLink>
 
     <NuxtLink to="/cart" class="font-semibold text-slate-700">
+      <span v-show="showAddedToCartAlert" class="mr-1 text-base text-slate-700">
+        Added to cart!
+      </span>
+
       <span class="mr-1 text-slate-700">Cart</span>
 
       <span class="rounded bg-slate-600 px-1 text-sm text-slate-100">
@@ -17,12 +21,15 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { useCartStore } from '@/stores/cart'
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
+
+const showAddedToCartAlert = ref(false)
 
 const { itemCount } = storeToRefs(useCartStore())
 watch(itemCount, (oldCount, newCount) => {
   if (oldCount > newCount) {
-    // TODO: animate something in the header
+    showAddedToCartAlert.value = true
+    setTimeout(() => (showAddedToCartAlert.value = false), 3000)
   }
 })
 </script>
