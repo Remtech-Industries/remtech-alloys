2d4a5b3af41833e89a5c1f860991194c2f2998ed
Trevor <treviguy57@gmail.com>
2023-04-28 14:32:12 -0400

    revert

--- components/PricingTable.vue
+++ components/PricingTable.vue
@@ -12,11 +12,7 @@
     <tbody>
       <tr v-for="row in items" :key="row.id">
         <td class="border-b px-6 py-2 font-medium text-slate-700">
-          <p>{{ row.title }}</p>
-
-          <p>
-            {{ isDefined(row.length) ? `Length: ${row.length} mm` : '' }}
-          </p>
+          {{ row.title }}
         </td>
         <td class="border-b px-6 py-2 text-right font-medium text-slate-700">
           {{ row.each }}
@@ -44,7 +40,6 @@
 </template>
 
 <script setup lang="ts">
-import { isDefined } from '@vueuse/core'
 import { computed } from 'vue'
 
 import { formatMoney } from '@/utils/format-money'
