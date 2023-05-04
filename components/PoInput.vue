26f080153518548ce0e49ab33dd4b432b5adf649
Trevor <treviguy57@gmail.com>
2023-05-04 16:06:59 -0400

    make component

---
+++ components/PoInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex">
+    <div
+      class="whitespace-nowrap rounded-l bg-slate-700 px-2 py-1 text-slate-50"
+    >
+      PO #
+    </div>
+
+    <input
+      type="text"
+      placeholder="(optional, for reference only)"
+      class="w-full border px-2 py-1 shadow-inner focus:outline-none"
+      @input="emit('update', ($event.target as HTMLInputElement).value)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{ (e: 'update', value: string): void }>()
+</script>
