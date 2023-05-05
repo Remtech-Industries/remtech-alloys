3e3b0a73d3c51a4570fa6a096f351dc04812c1c6
Trevor <treviguy57@gmail.com>
2023-05-05 09:45:17 -0400

    make two way binding

--- components/PoInput.vue
+++ components/PoInput.vue
@@ -7,14 +7,18 @@
     </div>
 
     <input
+      :value="modelValue"
       type="text"
       placeholder="(optional, for reference only)"
       class="w-full rounded-r border px-2 py-1 shadow-inner focus:outline-none"
-      @input="emit('update', ($event.target as HTMLInputElement).value)"
+      @input="
+        emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
     />
   </div>
 </template>
 
 <script setup lang="ts">
-const emit = defineEmits<{ (e: 'update', value: string): void }>()
+defineProps<{ modelValue: string }>()
+const emit = defineEmits<{ (e: 'update:modelValue', value: string): void }>()
 </script>
