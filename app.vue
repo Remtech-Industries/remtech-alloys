71365fb61f4d4fa1257b336919662f7ac2396b23
John Neusteter <jneusteter@gmail.com>
2023-06-26 07:39:19 -0400

    remove password

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="show" class="h-screen bg-slate-50">
+  <div class="bg-slate-50">
     <div class="flex justify-center bg-yellow-500 px-2">
       <div class="font-bold">
         Currently only shipping within Canada. USA coming soon. Rem-Tech Alloys
@@ -12,29 +12,11 @@
       <NuxtPage />
     </div>
   </div>
-
-  <div class="m-auto flex w-96 flex-col text-center" v-else>
-    <div class="text-2xl font-extrabold">Under Construction</div>
-    <input
-      class="w-96 rounded border border-slate-500"
-      placeholder="Enter password"
-      autofocus
-      v-model="pass"
-      type="text"
-    />
-  </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
-import { ref, computed } from 'vue'
 import { useCartStore } from '@/stores/cart'
 const { getCart } = useCartStore()
 getCart()
-
-const pass = ref('')
-
-const show = computed(() => {
-  return pass.value === '123'
-})
 </script>
