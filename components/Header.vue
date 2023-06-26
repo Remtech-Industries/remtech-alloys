a336075790d90ca7be2a876e96c9f2228c379729
Trevor Thiessen <treviguy57@gmail.com>
2023-06-26 07:30:46 -0400

    add linkedin link (#54)

--- components/Header.vue
+++ components/Header.vue
@@ -35,6 +35,32 @@
 
             <div class="ml-1 tracking-widest">(519) 773-3455</div>
           </div>
+
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/company/rem-tech-alloys-inc/"
+            class="flex cursor-pointer items-baseline"
+          >
+            <span class="mr-1.5">LinkedIn</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+              fill="white"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+              />
+
+              <path
+                fill-rule="evenodd"
+                d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+              />
+            </svg>
+          </a>
         </div>
 
         <div class="mt-4 flex justify-end">
