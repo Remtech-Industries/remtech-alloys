594c37483e8ad946a111092ff10cd739484c4b32
John Neusteter <jneusteter@gmail.com>
2023-07-26 09:34:07 -0400

    add site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: 'https://remtechalloys.com',
+  },
   imports: { autoImport: false },
   modules: [
     '@nuxtjs/tailwindcss',
