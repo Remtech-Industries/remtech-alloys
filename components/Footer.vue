3f672363d050374f39906b7562744fe011240f74
John Neusteter <jneusteter@gmail.com>
2023-09-18 16:09:08 -0400

    working example

---
+++ components/Footer.vue
@@ -0,0 +1,92 @@
+<template>
+  <footer
+    class="mt-auto h-20 w-full bg-slate-900 p-5 text-sm font-light text-slate-100"
+  >
+    <div class="flex h-full w-full flex-col gap-3">
+      <div class="flex items-center justify-center gap-10">
+        <div @click="showPolicy('privacyPolicy')">Privacy</div>
+        <div @click="showPolicy('termsOfService')">Terms of Service</div>
+        <div @click="showPolicy('shippingPolicy')">Shipping Policy</div>
+        <div @click="showPolicy('refundPolicy')">Return Policy</div>
+        <div>Contact Us</div>
+      </div>
+      <div class="text-center">
+        &copy; 2023 Rem-Tech Alloys Inc. All rights reserved.
+      </div>
+    </div>
+
+    <Dialog
+      :visible="!!words"
+      modal
+      @update:visible="words = null"
+      :header="header"
+      :style="{ width: '50vw' }"
+    >
+      <p v-html="words"></p>
+    </Dialog>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import Dialog from 'primevue/dialog'
+import { useShopifyUrl, useShopifyOptions } from '@/composables/useShopify'
+import { useFetch, ref } from '#imports'
+import type { ShopifyResponse, Shop } from '@/utils/types'
+
+type ShopPolicyResponse = ShopifyResponse<{
+  shop?: Pick<
+    Shop,
+    'refundPolicy' | 'privacyPolicy' | 'shippingPolicy' | 'termsOfService'
+  >
+}>
+
+type PolicyName =
+  | 'refundPolicy'
+  | 'privacyPolicy'
+  | 'shippingPolicy'
+  | 'termsOfService'
+
+const words = ref<string | null>(null)
+const header = ref('')
+
+const query = `
+query {
+  shop {
+    privacyPolicy {
+      body
+      title
+    }
+    refundPolicy {
+      title
+      body
+    }
+    shippingPolicy {
+      body
+      title
+    }
+    termsOfService {
+      body
+      title
+    }
+  }
+}
+`
+
+async function showPolicy(handle: PolicyName) {
+  if (!data.value) await execute()
+
+  const policy = data.value?.data?.shop?.[handle]
+  if (policy) {
+    words.value = policy.body
+    header.value = policy.title
+  }
+}
+
+const { data, execute } = useFetch<ShopPolicyResponse>(useShopifyUrl(), {
+  ...useShopifyOptions(query),
+  key: 'shopPolicy',
+  lazy: true,
+  server: false,
+  immediate: false,
+})
+</script>
--- utils/tailwind.ts
+++ utils/tailwind.ts
@@ -28,5 +28,34 @@ export const tailwind = {
         'focus:outline-none focus:ring-2 ring-slate-300'
       ]
     }
+  },
+  dialog: {
+    header: 'flex items-center justify-between shrink-0 bg-slate-50 text-slate-800 border-t-0 rounded-tl-lg rounded-tr-lg p-6',
+    headerTitle: 'font-bold text-lg',
+    headerIcons: 'flex items-center',
+    closeButton: {
+      class: [
+        'flex items-center justify-center overflow-hidden relative',
+        'w-8 h-8 text-gray-500 border-0 bg-transparent rounded-full transition duration-200 ease-in-out mr-2 last:mr-0',
+        'hover:text-gray-700 hover:border-transparent hover:bg-gray-200',
+        'focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]', // focus
+      ]
+    },
+    closeButtonIcon: 'w-4 h-4 inline-block',
+    content: {
+      class: [
+        'overflow-y-auto',
+        'bg-white text-gray-700 px-6 pb-8 pt-0',
+        'rounded-bl-lg rounded-br-lg',
+      ]
+    },
+    footer: 'shrink-0 border-t-0 bg-white text-gray-700 px-6 pb-6 text-right rounded-b-lg',
+    mask: 'transition duration-200 bg-black/40',
+    transition: {
+      enterFromClass: 'opacity-0 scale-75',
+      enterActiveClass: 'transition-all duration-200 ease-out',
+      leaveActiveClass: 'transition-all duration-200 ease-out',
+      leaveToClass: 'opacity-0 scale-75'
+    }
   }
 }
