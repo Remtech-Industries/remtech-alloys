fef15fcafb7c9fdcabe0e56ab1005115053ee2f0
Trevor <treviguy57@gmail.com>
2025-03-07 09:36:54 -0500

    add back rfq page

---
+++ pages/rfq/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mx-auto max-w-screen-md pt-6">
+    <h1 class="mb-10 text-3xl font-bold text-slate-700">
+      Submit orders as a request for quote (RFQ)
+    </h1>
+
+    <p class="text-slate-700">
+      Send your request for quote to <strong>rfq@remtechalloys.com</strong> or
+      via telephone at (<strong>519) 773-3455</strong>
+    </p>
+  </div>
+</template>
