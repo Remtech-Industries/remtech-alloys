c1f716063cfe3d9414d31cb7b9a49aaf56898181
John Neusteter <jneusteter@gmail.com>
2025-03-31 13:38:08 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# prevent the script from continuing if any command fails
+set -euo pipefail
+
+# pull latest version of site
+git pull origin main
+
+mise exec node -- npm install
+
+# build site with the low priority to not interfere with e1 important stuff
+nice -n 19 mise exec node -- npm run generate
+
+# upload site
+rsync -avz --delete .output/public/ e1@data.remtechalloys.com:/home/e1/staging.remtechindustries.com
